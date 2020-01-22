fefd237ee92d856f02613c65df91b4cc9cedc587
Gump <hg151515@ohio.edu>
2020-01-22 10:17:10 -0500

    Add project files.

---
+++ HG151515_MIS4200/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HG151515_MIS4200.MvcApplication" Language="C#" %>
---
+++ HG151515_MIS4200/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HG151515_MIS4200.Startup))]
+namespace HG151515_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
