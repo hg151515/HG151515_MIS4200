60db51bc7be7e6aa1c99f1e505267d5b18ab09e6
Hannah Gump <hg151515@ohio.edu>
2020-02-27 19:25:01 -0500

    Entered data annotations SD3

--- HG151515_MIS4200/Models/Student.cs
+++ HG151515_MIS4200/Models/Student.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 
 
 namespace HG151515_MIS4200.Models
 {
     public class Student
     {
+        public System.Guid SID { get; set; }
+
+        [StringLength(20)]
+        [Required]
         public int studentID { get; set; }
+
+        [Display(Name = "First name")]
+        [Required(ErrorMessage = "First name is required!")]
+        [StringLength(20)]
         public string studentfirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        [Required(ErrorMessage = "Last name is required!")]
+        [StringLength(20)]
         public string studentlastName { get; set; }
+
+        [Display(Name = "Enter student email address")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
         public string studentEmail { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = "Student first name is required")]
+        [StringLength(20)]
         public string studentPhone { get; set; }
+
+        [Display (Name ="Start date of enrollment at University")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime enrollDate { get; set; }
 
         //add any other fields as appropriate
@@ -27,4 +54,5 @@ namespace HG151515_MIS4200.Models
 
 
     }
+    
 }
